9066e46658a618bb3b115a31f9e4d9c0117cffaf
Kotofeishe <ksenia.migunova91@yandex.ru>
2024-08-18 21:58:09 +0300

    well done task: morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,9 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let n = expr.length/10;
+    let arrayResult = [];
+    let finishResult = '';
+    let k = 0;
+    for (let i = 0; i < n; i++) {
+        arrayResult = [...arrayResult, expr.slice(k, k+10)]
+        k = k + 10;
+    }
+    let arrayWithoutZero = '';
+    let arrayMorse = '';
+    arrayResult.forEach(value => {
+        if (value === '**********') {
+            finishResult = finishResult + ' ';
+        }
+        else {arrayWithoutZero = (value.slice(value.indexOf('1')));
+        for (let i = 0; i < arrayWithoutZero.length; i = i + 2) {
+            if(arrayWithoutZero.slice(i, i+2) === '10') {
+                arrayMorse = arrayMorse + '.';
+            }
+            else {
+                arrayMorse = arrayMorse + '-';
+            }
+        }
+        finishResult = finishResult + MORSE_TABLE[arrayMorse];
+        arrayMorse = '';
+}})
+    return finishResult;
 }
 
 module.exports = {
     decode
 }
+
+console.log (decode("0000001111**********0000000010"));
